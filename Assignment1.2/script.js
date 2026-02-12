bb88f46b6b1ef71f254f1f05dcbe59c2af41dae6
SubhranilSD <bijaya9932343826@gmail.com>
2026-02-12 21:15:12 +0530

    added full js

--- Assignment1.2/script.js
+++ Assignment1.2/script.js
@@ -22,19 +22,38 @@ let sampleEvent = [
         description: "..." // Code continues below visible area
     }
 ];
+
+
+
 //Create event card which contains the user data and we store it inside a div
 function createEventCard(eventData) {
-    const eventCard = document.createElement("div");
-    eventCard.classList.add("event-card");
-    eventCard.innerHTML = `
+    const Card = document.createElement("div");
+    //adding class name dynamically
+    Card.className = "event-card";
+    Card.innerHTML = `
         <h3>${eventData.title}</h3>
         <p><strong>Date:</strong> ${eventData.date}</p>
         <p><strong>Category:</strong> ${eventData.category}</p>
         <p><strong>Description:</strong> ${eventData.description}</p>
     `;
-    return eventCard;
+    return Card;
 }
-//Add event
+
+
+
+//Add event append
+function addEvent(eventData) {
+    const emptyState = document.querySelector(".empty-state");
+    if (emptyState) {
+        emptyState.remove();
+    }
+    const eventCard = createEventCard(eventData);
+    eventContainer.appendChild(eventCard);
+}
+
+
+
+//add event
 eventForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const eventData = {
@@ -44,4 +63,28 @@ eventForm.addEventListener("submit", (event) => {
         description: eventDescription.value
     };
     addEvent(eventData);
-});
+})
+
+
+eventContainer.addEventListener("click", (event) => {
+    const Card = event.target.closest(".event-card");
+    console.log(Card);
+    if (event.target.classList.contains("delete-btn")) {
+        Card.remove();
+    }
+    if (!eventContainer.querySelector(".event-card")) {
+        eventContainer.innerHTML = "<div class='empty-state'>No events yet. Add your first event!</div>";
+    }
+})
+
+addSampleBtn.addEventListener("click", () => {
+    sampleEvent.forEach((addEvent))
+})
+
+
+clearAllBtn.addEventListener("click", () => {
+    eventContainer.innerHTML = "<div class='empty-state'>No events yet. Add your first event!</div>";
+
+})
+
+
